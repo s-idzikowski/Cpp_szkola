8baac6ec37c63c72c644fca2be483ec454df2988
Slawomir Idzikowski <s.idzikowski@hotmail.com>
2019-10-31 20:26:42 +0100

    cw1-zadanie1

---
+++ c++_szkola/cw1_zadanie2.cpp
@@ -0,0 +1,21 @@
+/*
+Program obliczaj¹cy wartoœci objêtoœci i pola powierzchni ca³kowitej nastêpuj¹cych bry³:
+a) graniastos³up prosty o podstawie trapezu równoramiennego
+b) ostros³up prosty o podstawie prostok¹ta
+c) walec
+*/
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main() {
+	cout << "Program obliczajacy wartosci objetosci i pola powierzchni calkowitej\n"
+		<< "--------------------------------------------------------------------\n"
+		<< "Podaj bryle\n"
+		<< "a - graniastoslup prosty o podstawie trapezu rownoramiennego\n"
+		<< "b - ostroslup prosty o podstawie prostokata\n"
+		<< "c - walec\n";
+	string wybor;
+	cin >> wybor;
+	cout << "Wybrano: " << wybor;
+
+}
